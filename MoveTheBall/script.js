583cc4c9985f7bc0fbe29dd3f393acc8dd0d8538
Abhaydutt2003 <dutt.abhay@gmail.com>
2023-07-13 22:41:17 +0530

    Update script.js

--- MoveTheBall/script.js
+++ MoveTheBall/script.js
@@ -6,6 +6,9 @@ var keyPressHappened = false;
 var leftPos = 0;
 var topPos = 0;
 
+let inWidth = window.innerWidth;
+let inHeight = window.innerHeight;
+
 function fn(event) {
     if (keyPressHappened == false) {
         keyPressHappened = true;
@@ -13,17 +16,18 @@ function fn(event) {
         console.log(key);
         var interval = setInterval(fn2, 10);
         function fn2() {
-            if (key == 'd' || key == 'D') {
-                leftPos++;
+            let current = ball.getBoundingClientRect();
+            if ((key == 'd' || key == 'D') && (ball.offsetWidth + current.left <= inWidth)) {
+                leftPos += 0.5;
                 ball.style.left = leftPos + '%';
-            } else if (key == 'a' || key == 'A') {
-                leftPos--;
+            } else if ((key == 'a' || key == 'A') && (current.left >= 0)) {
+                leftPos -= 0.5;
                 ball.style.left = leftPos + '%';
-            } else if (key == 'w' || key == 'W') {
-                topPos--;
+            } else if ((key == 'w' || key == 'W') && (current.top >= 0)) {
+                topPos -= 0.5;
                 ball.style.top = topPos + '%';
-            } else if (key == 's' || key == 'S') {
-                topPos++;
+            } else if ((key == 's' || key == 'S') && (current.top + ball.offsetHeight <= inHeight)) {
+                topPos += 0.5;
                 ball.style.top = topPos + '%';
             }
         }
